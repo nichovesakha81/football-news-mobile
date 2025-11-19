f13ec5f2c4f7a1273482d59050b5e12fccdc38c7
nichovesakha81 <studynicho@gmail.com>
2025-11-19 21:12:22 +0700

    trigger push

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
 import 'package:football_news/screens/register.dart';
+// trigger
 
 void main() {
   runApp(const LoginApp());
